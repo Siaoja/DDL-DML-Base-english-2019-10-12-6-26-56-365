1524404f036e292bab3058ce71c1a4753f8516a4
Siaoja <1610692147@qq.com>
2020-06-24 20:56:49 +0800

    complete exercise

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **Database level:**
 -- Display all the database
-
+show databases;
 -- Enter a certain database
-
+use student_examination_sys(数据库名字);
 -- Create a database
-
+create database student_examination_sys(数据库名字);
 -- Create the database of the designated character set
-
+create database student_examination_sys(数据库名字) character set utf8;
 -- Display the creation information fo the database
-
+show create database student_examination_sys(数据库名字);
 -- Revise the codes of the database
-
+alter database student_examination_sys(数据库名字) character set gb2312;
 -- Delete a database
-
+drop database db_name;
 -- **Table level**
 -- Revise table name
-
+alter table student(修改前的表名) rename studentB(修改后的表名);
 -- Revise the field's data type
-
+alter table student(表名) change sex(字段名) int(1)(修改后的数据类型);
 -- Revise field name
-
+alter table student(表名) change sex(修改前字段名) sex1(修改后字段名);
 -- Add field
-
+alter table student(表名) add level(添加的字段名) int(1)(数据类型);
 -- Delete field
-
+alter table student(表名) drop level(删除的字段名);
 -- Revise the table's storage engine
-
+alter table student(表名) engine=MyISAM;
 -- Delete the table's foreign key restriant
-
+alter table student(表名) drop foreign key fk_name(外键名);
 -- Delete a table
+drop table student(删除的表名); 
--- dml.sql
+++ dml.sql
@@ -1,7 +1,15 @@
 -- Insert record
-
+--insert into 表名values (字段值1,字段值2,...,字段值n);
+insert into student values ('003','zhangsan',16,'male');
+--insert into 表名(字段1,字段2,....,字段n) values (字段值1,字段值2,...,字段值n);
+insert into student(id,name,age) values('004','lisi',20);
+-- 默认插入全部字段/列；必须保证values后面的内容的类型和顺序表结构中的字段顺序一致；
 -- Revise record
-
+--update 表名 set 字段名1=字段值1[where 条件]；
+update student set age=21 where id='004';
 --  Delete record
-
+--delete from 表名 [where 条件]
+delete from student where id='004';
 -- Search record
+--SELECT * FROM tablename[WHERE CONDITION]
+select * from student;
